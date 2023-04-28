dfbb6680c7b367a43603071d975e6d3755c95b9f
spurlingjohna <andrewspurling777@gmail.com>
2023-04-27 19:14:12 -0700

    commit

---
+++ Projects/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Spurling
+ *
+ * Created on April 27, 2023, 7:06 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
